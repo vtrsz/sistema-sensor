f605b3c8f50b3d6c3162f759b6d64d90f4d051fa
Vitor Souza <vitorsouzaribeiro1@gmail.com>
2023-12-08 20:22:49 -0300

    feat: backend project base

---
+++ backend/src/test/java/com/vtrsz/sistema_sensor/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtrsz.sistema_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
