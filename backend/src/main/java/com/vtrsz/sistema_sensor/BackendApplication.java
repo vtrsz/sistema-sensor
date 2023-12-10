5e50d2f83e0f304a00299d1b94b229aab2e6184c
Vitor Souza <vitorsouzaribeiro1@gmail.com>
2023-12-09 22:02:22 -0300

    feat(swagger): added config and docs in /api/docs

--- backend/src/main/java/com/vtrsz/sistema_sensor/BackendApplication.java
+++ backend/src/main/java/com/vtrsz/sistema_sensor/BackendApplication.java
@@ -1,9 +1,16 @@
 package com.vtrsz.sistema_sensor;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
+@OpenAPIDefinition(info = @Info(title = "API", version = "1.0", description = "API da Linha de Produção"))
 public class BackendApplication {
 
     public static void main(String[] args) {
